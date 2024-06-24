1031dc933ea60f016783abc6d887beb161066471
chaofan <chaofanat@qq.com>
2024-06-24 19:40:56 +0800

    feat(migrate): 添加静态文件收集和超级用户创建至迁移数据库脚本
    在migratedatabase.bat脚本中，新增了python manage.py collectstatic命令用于收集静态文件。同时，将创建超级用户的命令从HEAD分支中移除，并保留了其在脚本中的位置，以备后续需要。这些改动旨在优化数据库迁移过程，确保静态文件的正确部署和超级用户的创建。

--- migratedatabase.bat
+++ migratedatabase.bat
@@ -1,9 +1,10 @@
 python manage.py makemigrations
 python manage.py migrate
-<<<<<<< HEAD
-python3 manage.py createsuperuser
-=======
+
+python manage.py collectstatic
 
 @REM 创建超级用户
 python manage.py createsuperuser
->>>>>>> 0d0559449b0be96b15e36cffd8b52ab8600e61c3
+
+
+python run.py
