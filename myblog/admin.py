2ecac19c4d6426f20371974af9b2250fd33caa26
chaofan <chaofanat@qq.com>
2024-08-18 23:16:39 +0800

    ```admin 为Contact模型添加自定义管理界面 ```

--- myblog/admin.py
+++ myblog/admin.py
@@ -6,9 +6,15 @@ from django.contrib import admin
 from .models import Blog, Contact,Category,Tag,FeaturedBlog
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message', 'time')
+    list_filter = ('name', 'email')
+    search_fields = ('name', 'email', 'message')
+    readonly_fields = ('time',)
+
 
 admin.site.register(Blog)
-admin.site.register(Contact)
+admin.site.register(Contact,ContactAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(FeaturedBlog)
